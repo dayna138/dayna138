6b70bbf1f20463b5187bf0f1dc1e35384a0b72b0
Dayna <dayna.liao@gmail.com>
2019-07-25 16:14:33 +0800

    0725更新

--- p1.py
+++ p1.py
@@ -1,8 +1,26 @@
 # coding: utf-8
-testcase=int(input())
+testcase=4
 
 for i in range(testcase):
-    line = input()
-    a,b = line.split()
-    s = int(a,2)+int(b,2)
-    print(a,b,bin(s)[2:])
+    number = input()
+    a,b,c = number.split()
+    a,b,c = int(a), int(b), int(c) # a,b,c = map(int,line)
+    if a!=0: # if a:
+        a=1
+    if b!=0:
+        b=1
+    
+    fail=True
+    if (a & b==c):
+        print("AND")
+        fail = False
+    if (a or b==c):
+        print("OR")
+        fail = False
+    if (a ^ b==c):
+        print("XOR")
+        fail = False
+    if fail:
+    # else:
+        print("IMPOSSIBLE") # 若縮排相同 會在只判斷上一行的if情況下反應
+    
